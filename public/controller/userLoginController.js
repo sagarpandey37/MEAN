ec48bbdd711f4d6cc23095c4cef1fb8da534c0b1
Sagar Pandey <sagar.pandey@innovaccer.com>
2019-09-28 03:56:37 +0530

    login button name is dynamic now

--- public/controller/userLoginController.js
+++ public/controller/userLoginController.js
@@ -1,11 +1,15 @@
 app.controller("userLoginController", function ($scope,$localStorage,$window,xoloCarfactory) {
     
-    if($localStorage.userData.length >0){
+    if($localStorage.userData){
 		let localstorageData = JSON.parse($localStorage.userData);
             $scope.username = localstorageData['name']
             $scope.email = localstorageData['email'];
             $scope.phone = localstorageData['phone'];
+			$scope.button_label = 'login to continue'
         }
+	else{
+		$scope.button_label = 'signup'
+	}
       
     $scope.cabConfirmation = function(){
 		let userObject = xoloCarfactory.createUser($scope.username,$scope.phone,$scope.email);
